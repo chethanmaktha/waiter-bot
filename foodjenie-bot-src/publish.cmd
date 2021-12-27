88fe4aba421d7d638e6953755103688fbda7c349
chethanmaktha <88796663+chethanmaktha@users.noreply.github.com>
2021-12-27 08:25:13 -0800

    Add files via upload

---
+++ foodjenie-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\foodjenie-bot-b591-Web-Deploy.pubxml -p:Password=7uuMy9ZGLdejEWwvjBYogvDjRHqiCsd2org0C8fjrMRotn9TfiralBpKNiX8
+
